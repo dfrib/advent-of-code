cafb117d6597dfe11dcab2bd4315bcd9fcbf7999
dfrib <david.n.friberg@gmail.com>
2017-12-18 22:59:09 +0100

    [2017] Day 17 (cleanup)

--- 2017/Day17_Spinlock.swift
+++ 2017/Day17_Spinlock.swift
@@ -2,31 +2,16 @@
 // Swift 4
 // ------------------------------------------------------------------ //
 let puzzleInput = 324
-
-extension Array {
-    /*
-     mutating func rotateLeft(by n: Int) {
-     let nt = n % count
-     self = Array(self[nt..<count] + self[0..<nt])
-     }*/
-    mutating func rotateToLast(idx: Int) {
-        let i = idx % count
-        if i != count - 1 { self = Array(self[(i+1)...] + self[0...i]) }
-    }
-    mutating func reverse(upTo n: Int) {
-        if n >= count { self.reverse() }
-        else { self = Array(self[0..<n].reversed() + self[n..<count]) }
-    }
-    mutating func insert(_ newElement: Element, after: Int) {
-        if after >= count - 1 { self.append(newElement) }
-        else { insert(newElement, at: after + 1) }
-    }
-}
-
 let steps = puzzleInput
 
 // Part 1: always rotate the new current position to last.
 // (Every inserted value will be an appended one; after rotation).
+extension Array {
+    mutating func rotateToLast(idx: Int) {
+        let i = idx % count
+        if i != count - 1 { self = Array(self[(i+1)...] + self[0...i]) }
+    }
+}
 var buffer = [0]
 for i in 1...2017 {
     buffer.rotateToLast(idx: steps - 1)
